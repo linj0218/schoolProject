369e0e26919f09c34481ff9ed54c0e75375434f8
j.lin <j.lin@baisonmail.com>
2017-11-05 15:31:46 +0800

    增加登录

---
+++ schoolProject/src/pages/login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="body" v-cloak>
+    <div class="bg"></div>
+    <div class="form">
+      <div class="form_title">
+        <span></span>
+        <span>Welcome to LFS Portal</span>
+      </div>
+      <div class="form_cont">
+        <input class="form-control" type="text" placeholder="User" v-model='name'>
+        <input class="form-control" type="password" placeholder="Password" v-model='pwd'>
+      </div>
+      <div class="forget_pwd">
+        <a href="javascripe:void(0);">Forget your Password?</a>
+      </div>
+      <div class="login_btn">
+        <button type="button" class="btn btn-primary btn-block" @click='login()'>
+          Log in
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data () {
+    return {
+      name: '',
+      pwd: ''
+    }
+  },
+  methods: {
+    login () {
+      if (this.name === '' || this.pwd === '') {
+        return false;
+      }
+      let self = this
+      // TODO 填入正确地址，参数
+      this.$http.post('/queryWeekEvents', {
+        name: this.name,
+        password: this.pwd
+      }).then((res) => {
+        // TODO 处理返回数据
+        self.$router.push({ path: '/' })
+      })
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  #body{height: 100%;background: #f5f5f5;overflow: auto;position: relative;}
+  .bg{height: 100%;}
+  .form{
+    position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);width: 400px;height: 380px;background: #fff;
+    .form_title{line-height: 150px;color: #2C66C2;font-size: 22px;text-align: center;}
+    .form_cont{
+      padding: 0 20px;
+      .form-control{background: transparent;}
+      .form-control:first-child{margin-bottom: 15px;}
+    }
+    .forget_pwd{padding: 10px 30px;text-align: right;}
+    .login_btn{padding: 20px;}
+  }
+</style>
--- schoolProject/src/router/index.js
+++ schoolProject/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 const Test = r => require.ensure([], () => r(require('@/pages/test')), 'main')
+const Login = r => require.ensure([], () => r(require('@/pages/login')), 'main')
 const Index = r => require.ensure([], () => r(require('@/pages/index')), 'main')
 const Homepage = r => require.ensure([], () => r(require('@/pages/homepage')), 'main')
 
@@ -13,6 +14,10 @@ export default new Router({
       path: '/test',
       name: 'test',
       component: Test
+    }, {
+      path: '/login',
+      name: 'login',
+      component: Login
     }, {
       path: '/',
       name: 'index',
