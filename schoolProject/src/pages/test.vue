f0eb79cadfe654d0640fcd62075d2c8d4ecb4d73
j.lin <j.lin@baisonmail.com>
2018-04-23 17:40:35 +0800

    增加富文本组件

--- schoolProject/src/pages/test.vue
+++ schoolProject/src/pages/test.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <div @click='testClick()'>{{msg}}</div>
+  <div style="padding: 20px;">
+    <h3>弹框</h3>
+    <button type="button" class="btn" @click='testClick()'>{{msg}}</button>
     <alert ref='dialog' :input-value='"这是一个提示框"'></alert>
+    <h3>富文本</h3>
+    <uedit></uedit>
   </div>
 </template>
 
 <script>
   import alert from '@/components/alert'
+  import uedit from '@/components/uedit'
   import {mapState, mapMutations} from 'vuex'
   export default {
     name: 'test',
     components: {
-      alert
+      alert, uedit
     },
     data () {
       return {
@@ -32,17 +36,20 @@
       ...mapMutations(['SET_TEST']),
       testClick () {
         this.SET_TEST('test' + Math.random())
-        /*
         this.$refs.dialog.showDialog().then((res) => {
           console.log(res)
           this.$refs.dialog.show = false
         })
-        */
       }
+    },
+    // 离开路由注销富文本对象
+    beforeRouteLeave (to, from, next) {
+      window.UE.delEditor('editor');
+      next();
     }
   }
 </script>
 
 <style lang='scss' scoped>
-  
+
 </style>
